7976e94c9519fd33fb809869d9f0d847fb690c70
yasse_cse <yasse0218@icloud.com>
2022-11-10 15:07:42 +0900

    a

--- 5/4.c
+++ 5/4.c
@@ -8,32 +8,28 @@ int main(void)
 
 	/* ポインタpにaのアドレスを代入 */
 	/* ここにコードを追加  */
-  *p = &a;
+    p = &a;
 
 	printf("(1) a=%3d b=%3d *p=%3d\n",a,b,*p);
-	printf("(1) &a=%3d &b=%3d p=%3d\n",&a,&b,p);
 
 	/* ポインタpの指す先の値に「30（整数値）」を代入 */
 	/* ここにコードを追加  */
-  &p = 30;
+    *p = 30;
 
-	printf("(2) a=%3d b=%3d p=%3d\n",a,b,*p);
-	printf("(2) &a=%3d &b=%3d p=%3d\n",&a,&b,p);
+	printf("(2) a=%3d b=%3d *p=%3d\n",a,b,*p);
 
 	/* aに「100（整数値）」をｂに「200（整数値）」を代入 */
 	/* ここにコードを追加  */
-  &a = 100;
-  &b = 200;
+    a = 100;
+    b = 200;
 
-	printf("(3) a=%3d b=%3d p=%3d\n",a,b,*p);
-	printf("(3) &a=%3d &b=%3d p=%3d\n",&a,&b,p);
+	printf("(3) a=%3d b=%3d *p=%3d\n",a,b,*p);
 
 	/* ポインタpにbのアドレスを代入 */
 	/* ここにコードを追加  */
-  *p = &b;
+    p = &b;
 
-	printf("(4) a=%3d b=%3d p=%3d\n",a,b,*p);
-	printf("(4) &a=%3d &b=%3d p=%3d\n",&a,&b,p);
+	printf("(4) a=%3d b=%3d *p=%3d\n",a,b,*p);
 	
 	return (0);
 }
