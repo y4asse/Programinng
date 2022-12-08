55381a531d74c7ad553dd341999f73f9a35f5188
y4asse <yasse0218@icloud.com>
2022-12-08 11:55:12 +0900

    a

--- 8/4.c
+++ 8/4.c
@@ -12,12 +12,22 @@ typedef struct{
 void swap(double *x,double *y)
 {
 	/* ここにコードを追加  */
+	double tmp;
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
 }
 
 /* char[]の値交換（sx[]とsy[]の値を交換） */
 void swaps(char sx[],char sy[])
 {
 	/* ここにコードを追加  */
+	char tmp[5];
+	for(int i=0;i<5;i++){
+		tmp[i] =sx[i];
+		sx[i] = sy[i];
+		sy[i] = tmp[i];
+	}
 
 }
 
@@ -34,8 +44,9 @@ int main(void)
 	//ファイルを開く
 	//ファイルがオープンできなかったらtrueとなる
 	/* ここにコードを追加  */
+	if((fp = fopen(filename, "r")) == NULL){
 		printf("ファイルをオープンできません。\n");
-	else{
+	}else{
 		//ファイルがオープンできた時の処理
 		int j;
 		int k;	
@@ -47,12 +58,23 @@ int main(void)
 		
 		//ソートを行う
 		/* ここにコードを追加  */
+		for(i=0;i<sizeof(ma)/sizeof(ma[0]);i++){
+			for(j=i+1;j<sizeof(ma)/sizeof(ma[0]);j++){
+				if(ma[i].height > ma[j].height){
+					swaps(ma[i].name, ma[j].name);
+					swap(&ma[i].height, &ma[j].height);
+					swap(&ma[i].weight, &ma[j].weight);
+				}
+			}
+		}
+
 	
 		//ソート結果を表示
 		printf("----------SortResult----------\n");
 		for(i=0;i<j;i++)
 			printf("%-10s %5.1f %5.1f\n",ma[i].name,ma[i].height,ma[i].weight);
 		fclose(fp);
+		
 	}
 
 	return (0);
